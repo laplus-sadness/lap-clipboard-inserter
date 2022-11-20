59c916bafe7f80dcf902bf85a9f1f1d541e59a71
laplus-sadness <118642172+laplus-sadness@users.noreply.github.com>
2022-11-20 13:24:50 -0300

    Fix multiline string error in background.ts

--- src/background.ts
+++ src/background.ts
@@ -10,9 +10,11 @@ chrome.browserAction.onClicked.addListener(() =>
     chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
         for (const tab of tabs) {
             if (tab.id === undefined) {
-                console.error(`Error when querying the tabs: ClipboardInserter
-                              doesn't work in windows that don't host content
-                              tabs (for example, devtools windows)`);
+                console.error(
+                    "Error when querying the tabs: ClipboardInserter " +
+                        "doesn't work in windows that don't host content " +
+                        "tabs (for example, devtools windows)"
+                );
             } else {
                 toggleTab(clipboardInserter, tab.id);
             }
